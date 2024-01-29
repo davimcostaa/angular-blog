cb10007f6dcd219b60ee61891fb5343114030c4b
Davi Costa <davim.costa@outlook.com>
2024-01-29 16:44:12 -0300

    Páginas dinâmicas.

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,21 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photoCover": "https://blog.frame.io/wp-content/uploads/2022/03/aotc-the-batman-cinematic-flare.jpg",
+    "title": "Fotografia do filme Batman é incrível",
+    "content": "Filme concorreu ao Oscar de melhor fotografia.",
+
+  },
+  {
+    "id": "2",
+    "photoCover": "https://cinepop.com.br/wp-content/uploads/2019/10/disney-plus.jpg",
+    "title": "Nova série da Marvel anunciada pelo Disney+",
+    "content": "lorem impsum"
+  },
+  {
+    "id": "3",
+    "photoCover": "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2023/05/beyonce-renaissance-tour.jpg",
+    "title": "Estreia do novo filme de Beyoncé",
+    "content": "lorem impsum"
+  },
+]
