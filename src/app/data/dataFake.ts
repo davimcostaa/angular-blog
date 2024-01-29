5fc7d3dcbed81222eed315e40762937a52be02ed
Davi Costa <davim.costa@outlook.com>
2024-01-29 16:55:55 -0300

    Página home dinâmica.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,19 +3,19 @@ export const dataFake = [
     "id": "1",
     "photoCover": "https://blog.frame.io/wp-content/uploads/2022/03/aotc-the-batman-cinematic-flare.jpg",
     "title": "Fotografia do filme Batman é incrível",
-    "content": "Filme concorreu ao Oscar de melhor fotografia.",
+    "content": "A fotografia do filme 'The Batman' de 2022 é verdadeiramente excepcional, elevando a experiência cinematográfica a um novo patamar. Sob a direção do renomado diretor de fotografia Greig Fraser, as imagens são magistralmente capturadas, destacando-se pela composição visual cuidadosa, uso envolvente da luz e sombras, e uma paleta de cores que cria uma atmosfera única. Cada cena é meticulosamente planejada, proporcionando uma estética visualmente impactante que complementa perfeitamente a narrativa sombria e intrigante do filme. A qualidade da fotografia em The Batman não apenas enriquece a experiência visual, mas também contribui significativamente para a construção do mundo imersivo e atmosférico que caracteriza o universo do Cavaleiro das Trevas.",
 
   },
   {
     "id": "2",
     "photoCover": "https://cinepop.com.br/wp-content/uploads/2019/10/disney-plus.jpg",
     "title": "Nova série da Marvel anunciada pelo Disney+",
-    "content": "lorem impsum"
+    "content": " Com entusiasmo e expectativa crescentes, a Disney+ anunciou a mais recente adição ao seu catálogo de séries Marvel: Echo. Baseada na personagem Maya Lopez, esta série promete ser uma jornada emocionante e envolvente, trazendo uma perspectiva única ao universo cinematográfico da Marvel. Echo destaca-se não apenas por sua trama intrigante, mas também pela representatividade que traz à tela. Ao explorar a história de Maya, uma heroína surda e habilidosa, a série oferece uma narrativa inclusiva e autêntica que ressoa com uma audiência diversificada."
   },
   {
     "id": "3",
     "photoCover": "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2023/05/beyonce-renaissance-tour.jpg",
     "title": "Estreia do novo filme de Beyoncé",
-    "content": "lorem impsum"
+    "content": "Com grande expectativa e antecipação, o filme Renaissance de Beyoncé fez sua estreia triunfante nos cinemas, levando os espectadores a uma jornada visual e musical única. Este projeto, que reflete a inovação artística e criativa da icônica artista, oferece uma experiência cinematográfica imersiva que transcende os limites tradicionais do entretenimento."
   },
 ]
